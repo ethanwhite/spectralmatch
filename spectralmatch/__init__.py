ec4f947d47137da5b221221c4a1a4078e9a63a56
cankanoa <cankanoa@gmail.com>
2025-04-17 13:13:09 -1000

    Remove not yet supported imports from `__init__.py`

--- spectralmatch/__init__.py
+++ spectralmatch/__init__.py
@@ -1,8 +1,6 @@
 from process import global_match, local_match
-from masking import create_cloud_mask, post_process_raster_cloud_mask
+
 __all__ = [
     "global_match",
     "local_match",
-    "create_cloud_mask",
-    "post_process_raster_cloud_mask",
 ]
